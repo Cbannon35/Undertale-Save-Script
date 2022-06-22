090b007eb80bf0b3db77c4ab3f4d6da45adbadae
Christopher Bannon <bannon.c.35@berkeley.edu>
2022-06-21 20:10:09 -0700

    v1.0.0-beta: have not exhaustively tested but think it is functional enough to be considered complete --> still have to add personal touches

--- script.py
+++ script.py
@@ -7,9 +7,11 @@ import sys
 #############
 # Variables #
 #############
+
+
 cwd = os.getcwd()
 args = sys.argv[1:]
-version = "0.5.1"
+version = "1.0.0-Beta"
 # Folder undertale stuff lives in (specifically on macOS + Steam)
 undertale_dir = os.path.expanduser("~/Library/Application Support/com.tobyfox.undertale/")
 if not os.path.isdir(undertale_dir):
@@ -20,6 +22,8 @@ if not os.path.isdir(undertale_dir):
 #############
 # Functions #
 #############
+
+
 def parse_args(args):
     """ Parses the arguments passed to the script. 
     Handles the cases of incorrect arg length and invalid args. 
@@ -50,9 +54,12 @@ def print_all_help():
         print("\n{}:\n{}".format(command, commands[command]))
     sys.exit(1)
 
+
 ###########################
 # Functions in "commands" #
 ###########################
+
+
 def help(args=[]):
     """ Prints the help for a command. Depends on the command passed. """
     commands = fetch_commands_help()
@@ -76,19 +83,31 @@ def print_version(args=[]):
     #TODO: funny stuff depending on num args ?
     print("Undertale Save Swapper v{}".format(version))
 
-def new_save(args):
+def save(args):
     """ Creates a new save file. """
     if len(args) == 1:
         save_name = args[0]
         save_path = os.path.join(cwd, save_name)
-        if not os.path.exists(save_path):
-            # create directory if it doesn't exist
-            os.makedirs(save_path)
+        # undertale_dir --> save_path
+        #            d1 --> d2
         # move all files from undertale to new save directory
-        for file in os.listdir(undertale_dir):
-            if file == "config.ini":
-                continue
-            os.rename(os.path.join(undertale_dir, file), os.path.join(save_path, file))
+        transfer(undertale_dir, save_path)
+    else:
+        print("Invalid number of arguments: {}".format(len(args)))
+        sys.exit(1)
+
+def load(args):
+    """ Loads a save file. """
+    if len(args) == 1:
+        load_name = args[0]
+        load_path = os.path.join(cwd, load_name)
+        # undertale_dir --> save_path
+        #            d1 --> d2
+        # move all files from undertale to new save directory
+        if is_valid_undertale_dir(load_path):
+            transfer(load_path, undertale_dir)
+        else:
+            sys.exit(1)
     else:
         print("Invalid number of arguments: {}".format(len(args)))
         sys.exit(1)
@@ -96,28 +115,21 @@ def new_save(args):
 def swap_save(args):
     """ Swaps the save file of undertale. """
     if len(args) == 2:
-        load_name = args[0]
-        load_path = os.path.join(cwd, load_name)
-        save_name = args[1]
-        save_path = os.path.join(cwd, save_name)
-        # Check if you are loading a valid save file
+        load_name, save_name = args[0], args[1]
+        load_path, save_path = os.path.join(cwd, load_name), os.path.join(cwd, save_name)
+        # Check if you are loading a valid file 
         if not os.path.exists(load_path):
             print("Invalid load file: {}".format(load_name))
             sys.exit(1)
 
         # move all files from undertale to new or existing save directory
-        if not os.path.exists(save_path):
-            # create directory if it doesn't exist
-            os.makedirs(save_path)
+        transfer(undertale_dir, save_path)
 
-        # move all files from undertale to new save directory
-        for file in os.listdir(undertale_dir):
-            if file == "config.ini":
-                continue
-            os.rename(os.path.join(undertale_dir, file), os.path.join(save_path, file))
         # move all files from load_path to undertale
-        for file in os.listdir(load_path):
-            os.rename(os.path.join(load_path, file), os.path.join(undertale_dir, file))
+        transfer(load_path, undertale_dir)
+    else:
+        print("Invalid number of arguments: {}".format(len(args)))
+        sys.exit(1)
 
 def play(args):
     """ Starts undertale. """
@@ -126,14 +138,18 @@ def play(args):
         if not os.path.exists(undertale):
             print("Could not find undertale. Please make Undertale is in your applications folder.")
             sys.exit(1) 
-        os.system("open {}".format(undertale))
+        else:
+            os.system("open {}".format(undertale))
     else:
         print("Invalid number of arguments: {}".format(len(args)))
         sys.exit(1)
 
+
 ####################
 # Helper Functions #
 ####################
+
+
 def fetch_commands_help():
     """ Returns a dictionary of commands and their descriptions. 
     For use in print_help(). """
@@ -156,10 +172,70 @@ def fetch_functions():
     # functions['command'] = function
     functions["help"] = help
     functions["version"] = print_version
-    functions["new"] = new_save
+    functions["save"] = save
+    functions["load"] = load
     functions["swap"] = swap_save
     functions["play"] = play
     return functions
+
+def transfer(d1, d2):
+    """ Moves the contents of d1 to d2. (Ignoring config file) """
+    if is_valid_undertale_dir(d1):
+        if not os.path.exists(d2):
+            # make d2 if it doesn't exist
+            os.makedirs(d2)
+        elif len(os.listdir(d2)) == 1 and os.listdir(d2)[0] == "config.ini":
+            pass # handle the case where undertale_dir should always have config.ini
+        elif len(os.listdir(d2)) > 0:
+             # if d2 exists and is not empty, ask user if they want to overwrite
+            warning_overwrite(d2)
+        # Move files over
+        for file in os.listdir(d1):
+            if file == "config.ini": # ignore config file --> should this be toggle-able?
+                continue
+            os.rename(os.path.join(d1, file), os.path.join(d2, file))
+    else:
+        print("Invalid directory: {}".format(d1))
+        sys.exit(1)
+
+def is_valid_undertale_dir(d):
+    """ Checks if d is a valid undertale directory. """
+    files = {"undertale.ini": 3, "file0": 17, "file9": 27}
+    if os.path.exists(d) and os.path.isdir(d):
+        d_as_list = os.listdir(d)
+        if len(d_as_list) >= 3: # should only have 3 files in undertale directory
+            #TODO: make a better file validity check
+            test = 0 # if 3 files are found test will add up to the correct number
+            for file in os.listdir(d):
+                if file in files:
+                    test += files[file]
+            if test == 47:
+                return True
+            else:
+                print("Invalid files in directory: {}\n{}".format(d, d_as_list))
+                return False
+        else:
+            print("Incorrect number of files in directory: {}".format(d))
+            return False
+    return False
+
+def warning_overwrite(d):
+    """ Prints a warning that the save directory is not empty. """
+    print("Warning: {} isn't empty. Potentially overwriting existing data.".format(d))
+    def wait_for_input():
+        """ Waits for user input. """
+        y_or_n = input("Continue? (y/n): ")
+        if y_or_n == "y":
+            print("Overwriting...")
+            return
+        elif y_or_n == "n":
+            print("Aborting...")
+            sys.exit(1)
+        else:
+            print("Invalid input: {}".format(y_or_n))
+            wait_for_input()
+    wait_for_input()
+
 
 #########################
 # Execute Script (Main) #
